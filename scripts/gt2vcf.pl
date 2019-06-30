7aadb3dbfdabd2e999d740feb27039de02d4f823
Eli-Meyer <elimeyer76@gmail.com>
2019-06-30 08:02:00 -0700

    fixed a bug in gt2vcf

--- scripts/gt2vcf.pl
+++ scripts/gt2vcf.pl
@@ -36,16 +36,16 @@ open(OUT, ">$opt_o");
 
 
 # add metadata lines to output file
-print "##fileformat=VCFv4.2\n";
+print OUT "##fileformat=VCFv4.2\n";
 @ta = localtime(time());
 @months = ("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
 @days = ("01", "02", "03", "04", "05", "06", "07", "08", "09", 10..30);
-print "##fileDate=", 1900+$ta[5], $months[$ta[4]], $days[$ta[3]-1], "\n";
-print "##source=",$scriptname, "\n";
-if (defined($rfile)) {print "##reference=", $rfile, "\n";}
+print OUT "##fileDate=", 1900+$ta[5], $months[$ta[4]], $days[$ta[3]-1], "\n";
+print OUT "##source=",$scriptname, "\n";
+if (defined($rfile)) {print OUT "##reference=", $rfile, "\n";}
 
 # INFO lines
-print "##INFO=<ID=NS,Number=1,Type=Integer,Description=\"Number of Samples With Data\">\n";
+print OUT "##INFO=<ID=NS,Number=1,Type=Integer,Description=\"Number of Samples With Data\">\n";
 
 # FILTER lines
 if (defined($ffile))
@@ -55,14 +55,14 @@ if (defined($ffile))
 		{
 		chomp;
 		@bits = split("\t", $_);
-		print "##FILTER=<ID=", $bits[0], ",Description=\"", $bits[1], "\">\n";
+		print OUT "##FILTER=<ID=", $bits[0], ",Description=\"", $bits[1], "\">\n";
 		}
 	}
 
 # FORMAT lines
-print "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n";
+print OUT "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n";
 
-print "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t";
+print OUT "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t";
 
 while(<IN>)
 	{
@@ -73,12 +73,12 @@ while(<IN>)
 
 	if ($rowcount==1)
 		{
-		print join("\t", @cols[2..$nom]), "\n";
+		print OUT join("\t", @cols[2..$nom]), "\n";
 		next;
 		}
 
 	$tagcount++;
-	print $cols[0], "\t", $cols[1], "\t", ".", "\t";
+	print OUT $cols[0], "\t", $cols[1], "\t", ".", "\t";
 
 # identify and print reference allele
 	$refstr = `grep $cols[0] -A 1 -m 2 $rfile | grep -v ">" | grep -v "-"`;
@@ -100,14 +100,14 @@ while(<IN>)
 	if ($refcall eq "N")
 		{
 		$refcall = $aaa[0];
-		print $refcall, "\t";
+		print OUT $refcall, "\t";
 		shift @aaa;
-		print join(",", @aaa), "\t";
+		print OUT join(",", @aaa), "\t";
 		}
 	else
 		{
-		print $refcall, "\t";
-		print join(",", @aaa), "\t";
+		print OUT $refcall, "\t";
+		print OUT join(",", @aaa), "\t";
 		}
 # make a hash of alleles
 	%ach = (); $allno = 0;
@@ -119,18 +119,18 @@ while(<IN>)
 		}
 
 # print empty genotype quality score
-	print ".", "\t";
+	print OUT ".", "\t";
 
 # print filter status (all passed; assumption of this script)
-	print "PASS\t";
+	print OUT "PASS\t";
 
 # print number of samples with genotypes called at this locus
-	print "NS=", $gtni, "\t";
+	print OUT "NS=", $gtni, "\t";
 
 # print FORMAT 
-	print "GT", "\t";
+	print OUT "GT", "\t";
 
-# print genotype data for each sample
+# print OUT genotype data for each sample
 
 	for ($a=2; $a<$nom; $a++)
 		{
@@ -138,17 +138,17 @@ while(<IN>)
 		if ($cols[$a] =~ / /)
 			{
 			@obsall = split(" ", $cols[$a]);
-			print $ach{$obsall[0]}, "/", $ach{$obsall[1]}, "\t";
+			print OUT $ach{$obsall[0]}, "/", $ach{$obsall[1]}, "\t";
 			}
 		elsif ($cols[$a] eq 0)
 			{
-			print "./.", "\t";
+			print OUT "./.", "\t";
 			}
 		else
 			{
-			print $ach{$cols[$a]}, "/", $ach{$cols[$a]}, "\t";	
+			print OUT $ach{$cols[$a]}, "/", $ach{$cols[$a]}, "\t";	
 			}
 		}
-	print "\n";
+	print OUT "\n";
 	}
 
